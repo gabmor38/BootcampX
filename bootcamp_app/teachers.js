3b0ec65b6e080dd8d8b4481e07db3e426a9d3825
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-07 22:24:08 -0400

    changed students and teachers to parameterized values

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,19 @@ const pool = new Pool({
 
 pool.connect();
 
-pool.query(` SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.id) as Total
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+const queryString = ` SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.id) as Total
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN Students ON student_id = students.id
 JOIN Cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 GROUP BY teacher, cohorts.name
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort} : ${row.teacher}`);
